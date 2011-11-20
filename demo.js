a367d087c24770065955cb6be38318a1b98fcef7
Petteri Hietavirta <petteri.hietavirta@gmail.com>
2011-11-20 23:33:54 +0200

    Flake nudging added.

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 function Demo() {
-	var MAX_FLAKES = 100;
+	var MAX_FLAKES = 500;
 	var flakes = [];
 	var tops = [];
 
@@ -13,27 +13,31 @@ function Demo() {
     var copyCtx = copyCanvas.getContext('2d');
 
     function demoStep() {
-    	updateFlakes();
     	if (flakes.length < MAX_FLAKES) spawnFlakes();
     	drawFlakes();
+    	updateFlakes();
     	requestAnimFrame(demoStep);
     }
 
     function updateFlakes() {
-    	
 		for (var i=0; i < flakes.length; i++) {
-		// if not moving, then paint to background and reuse 
+		// if hits the ground, then paint to background and reuse 
 			var flake = flakes[i];
-			if (flake.y > canvas_height-10 || Math.floor(flake.y) > tops[Math.floor(flake.x)]) {
-				tops[Math.floor(flake.x)]--;
-				copyCtx.fillRect(Math.floor(flake.x), Math.floor(flake.y),1,1); 
+			if  ((flake.y + flake.speed) > tops[Math.floor(flake.x)]) {
+				var nudge = checkAngle(Math.floor(flake.x));			
+				if (nudge != 0) {
+					flake.x = Math.floor(flake.x) + nudge;
+				} 
+				tops[Math.floor(flake.x)] = tops[Math.floor(flake.x)] - 1;
+				copyCtx.fillRect(Math.floor(flake.x), tops[Math.floor(flake.x)],1,1); 
 				flake.y = 0;
 				flake.x = Math.floor(Math.random()*canvas_width);
 				flake.ttl = 0;
+			} else {
+				flake.x += Math.sin((flake.ttl/200.0)*(1-flake.speed));
+				flake.y = flake.y + flake.speed;
+				flake.ttl++;
 			}
-			flake.y = flake.y + flake.speed;
-			flake.ttl++;
-			flake.x += Math.sin((flake.ttl/200.0)*(1-flake.speed));
 		}  
     }
     
@@ -58,8 +62,29 @@ function Demo() {
 
     function populateTops() {
     	for (var i=0; i < canvas_width; i++) {
-    		tops.push(119);
+    		tops.push(canvas_height-10);
 	    }
+    }
+    
+    function checkAngle(x) {
+    	if (x > 0 && x < canvas_width) {
+    		var left = tops[x-1];
+    		var right = tops[x+1];
+    		var current = tops[x];
+    		
+    		// lower than neighbours or level - lower has greater value 
+    		if (current >= left && current >= right) {
+    			return 0;
+    		// higher than neighbours, neighbours level
+    		} else if (left === right) {
+    			return  (Math.random() > 0.5) ? 1 : -1;
+    		} else if (left > right) {
+    			return -1;
+    		} else {
+    			return 1;
+    		}
+    	}
+    	return 0; 
     }
     
     function pixelData(data,x,y) {
